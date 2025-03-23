79b63cb521ae43bb1e42c55ee0da5ff510e59e1b
Miguel <miguelangeltaboada140@gmail.com>
2025-03-22 23:20:14 -0500

    Tu mensaje de commit

---
+++ src/main/java/SistemaDeInventario/SistemaDeInventario/SistemaDeInventarioApplication.java
@@ -0,0 +1,13 @@
+package SistemaDeInventario.SistemaDeInventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SistemaDeInventario/SistemaDeInventario/SistemaDeInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package SistemaDeInventario.SistemaDeInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
