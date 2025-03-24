c6f796610e82fc2a0029f84fd7c3f4e102fa3560
Miguel <miguelangeltaboada140@gmail.com>
2025-03-23 21:49:27 -0500

    mas avances

---
+++ src/main/java/SistemaDeInventario/SistemaDeInventario/Controller/DetalleDeProductos.java
@@ -0,0 +1,12 @@
+package SistemaDeInventario.SistemaDeInventario.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DetalleDeProductos {
+    @GetMapping("/DetalleDeProductos")
+    public String DetalleDeProductos(){
+        return "DetalleDeProductos";
+    }
+}
---
+++ src/main/java/SistemaDeInventario/SistemaDeInventario/Controller/ReporteDeVentas.java
@@ -0,0 +1,12 @@
+package SistemaDeInventario.SistemaDeInventario.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReporteDeVentas {
+    @GetMapping("/ReporteDeVentas")
+        public String ReporteDeVentas(){
+        return "ReporteDeVentas";
+        }
+}
